c7ecf5dd1b2801804b9cb0a95255622fba81461e
蘇元貞 <juliesu@suyuanzhens-MacBook-Pro.local>
2025-02-09 22:06:56 +0800

    package fixed, pnpm-lock.yaml added, page.tsx search removed

--- app/page.tsx
+++ app/page.tsx
@@ -35,10 +35,6 @@ export default function Page() {
             <a className="nav-link disabled" href="#">Disabled</a>
           </li>
         </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
       </div>
     </nav>
   );
